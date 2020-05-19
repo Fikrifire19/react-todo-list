5489dc3511992eb2127fb9dcf0125fae41abde94
Fikrifire19 <40727135+Fikrifire19@users.noreply.github.com>
2020-05-19 09:23:28 +0700

    paper component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from "react";
 import "./styles.css";
+import Paper from "./components/Paper";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <div className="frame">
-          <section className="header">
-            <button className="header-btn">Add</button>
-            <h1 className="header-title">To Do List</h1>
-            <button className="header-btn main-color-red">Clear</button>
-          </section>
-          <section className="add">
-            <form action="" className="add-form">
-              <input type="text" className="add-input" />
-              <button className="add-btn">Add</button>
-            </form>
-          </section>
-          <section className="todos">
-            <div className="todo">
-              <p className="text-todo">Learning React</p>
-            </div>
-          </section>
+    <Paper>
+      <section className="header">
+        <button className="header-btn">Add</button>
+        <h1 className="header-title">To Do List</h1>
+        <button className="header-btn main-color-red">Clear</button>
+      </section>
+      <section className="add">
+        <form action="" className="add-form">
+          <input type="text" className="add-input" />
+          <button className="add-btn">Add</button>
+        </form>
+      </section>
+      <section className="todos">
+        <div className="todo">
+          <p className="text-todo">Learning React</p>
         </div>
-      </div>
-    </div>
+      </section>
+    </Paper>
   );
 }
