4c7645e741da45ac98c126cef3682e1a7629718d
Fikrifire19 <40727135+Fikrifire19@users.noreply.github.com>
2020-05-19 08:49:57 +0700

    penambahan component dan style

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="container">
+        <div className="frame">
+          <section className="header">
+            <button className="header-btn">Add</button>
+            <h1 className="header-title">To Do List</h1>
+            <button className="header-btn main-color-red">Clear</button>
+          </section>
+          <section className="add">
+            <form action="" className="add-form">
+              <input type="text" className="add-input" />
+              <button className="add-btn">Add</button>
+            </form>
+          </section>
+          <section className="todos">
+            <div className="todo">
+              <p className="text-todo">Learning React</p>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
